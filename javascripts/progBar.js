3617e7403e1d97be833d1a02fba0b0b0853ef379
Barry <blrussell09@gmail.com>
2018-05-15 08:37:51 -0500

    added progress bar

--- javascripts/progBar.js
+++ javascripts/progBar.js
@@ -8,14 +8,14 @@ const progBarUp = () =>
     console.log(perc);
     $('#progress').width(`${perc}%`).text(perc);
     return perc;
-  };
+  }
   if ($('#Actors').find('input.clicked').length !== 0)
   {
     perc += 25;
     console.log(perc);
     $('#progress').width(`${perc}%`).text(perc);
     return perc;
-  };
+  }
   if ($('#Locations').find('input.clicked').length !== 0)
   {
     perc += 25;
